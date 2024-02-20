d838fa889e1f0ec9690adb5ebb53459d5c07bf3c
Tharnath Bagirathan <itstharnath@gmail.com>
2024-02-19 18:58:24 -0700

    Lab3 added with code for 3.1

---
+++ Lab3/MDK-ARM/RTE/_Lab3/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab3' 
+ * Target:  'Lab3' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
